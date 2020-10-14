7c8a62136f948269610bb7c75cb186c9b06b9f12
Diogo Luz <diogoseicaluz@gmail.com>
2020-10-14 01:22:20 +0100

    Added Landing Page to front-end

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; //import do react
+import ReactDOM from 'react-dom'; //integração do react com a DOM (árvore de elementos do HTML)
 import App from './App';
 
+//render recebe algumas tags de html ou componentes do react e coloca dentro do html (no root do index neste caso)
+//componentes de react são funções que retornam um .jsx
 ReactDOM.render(
   <React.StrictMode>
     <App />
