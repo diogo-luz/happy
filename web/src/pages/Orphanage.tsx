c50dd891fbb81447acfad582a3f96129d232f54e
Diogo Luz <diogoseicaluz@gmail.com>
2020-12-14 01:04:51 +0000

    🚑 Bugfix - Wrong variable type being used

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -17,7 +17,7 @@ interface Orphanage {
   about: string;
   instructions: string;
   opening_hours: string;
-  open_on_weekends: string;
+  open_on_weekends: boolean;
   images: Array<{
     id: number;
     url: string;
